08aba9aa36d43c6d06e444e4591c93d289cecde9
prostokot14 <86567361+prostokot14@users.noreply.github.com>
2023-06-02 17:30:20 +0200

    Initial Commit

---
+++ Project7/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project7
+//
+//  Created by Антон Кашников on 02.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
